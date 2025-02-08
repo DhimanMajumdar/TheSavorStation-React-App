d6a89ccd0621777c8413d262ab589e82019a67ec
DhimanMajumdar <dhimanmajumdar08233@gmail.com>
2025-02-08 17:54:51 +0530

    cors extension dependency removed

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,6 +1,7 @@
 
 export const IMG_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-export const SWIGGY_API="https://www.swiggy.com/dapi/restaurants/list/v5?lat=15.549441&lng=73.7534857&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+export const SWIGGY_API=`https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=15.549441&lng=73.7534857&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`
 export const LOGO_URL="https://img.freepik.com/free-vector/vintage-restaurant-menu_23-2147491098.jpg?t=st=1710864654~exp=1710868254~hmac=bc88d5e444cc9e4b93ae0cf2c9031d449283be79ec7519a0ee59fbe61f596fce&w=740"
 export const MENU_API =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=15.549441&lng=73.7534857&restaurantId=";
+  `https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=15.549441&lng=73.7534857&restaurantId=`;
+
